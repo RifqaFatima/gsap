abed1b20a54ce6020eee011851c4e0cd8c50a981
RifqaFatima <rifqa88077@gmail.com>
2026-05-20 12:27:56 +0530

    Initial Commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <h1 className="text-3xl text-indigo-300">Hello</h1>
+  )
+}
+
+export default App

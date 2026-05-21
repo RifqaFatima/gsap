9fd602e92cce61536da6814e37a55110107aea6f
RifqaFatima <rifqa88077@gmail.com>
2026-05-21 14:37:15 +0530

    Navbar implemented

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import gsap from 'gsap';
-import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from './components/Navbar.jsx';
+// import { ScrollTrigger, SplitText } from "gsap/all";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className ="flex-center h-[100vh]">
-      <h1 className="text-3xl text-indigo-300">Hello</h1>
-
-    </div>
+    <main>
+      <Navbar />
+    </main>
 
   )
 }

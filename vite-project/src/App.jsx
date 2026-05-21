b56932cbd1b3c2f91c64bfb2dac5d3dde9042e47
RifqaFatima <rifqa88077@gmail.com>
2026-05-21 17:53:44 +0530

    Implement the first part of the hero section

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import gsap from 'gsap';
 import Navbar from './components/Navbar.jsx';
+import Hero from './components/Hero.jsx';
 // import { ScrollTrigger, SplitText } from "gsap/all";
 
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -12,6 +13,8 @@ const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black"/>
     </main>
 
   )

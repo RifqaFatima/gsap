b114234becb5c2144af475b3009a41f3e42a7621
RifqaFatima <rifqa88077@gmail.com>
2026-05-23 21:32:53 +0530

    Implement Hero Scroll animation

--- vite-project/src/components/Hero.jsx
+++ vite-project/src/components/Hero.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import gsap from 'gsap';
 import {useGSAP} from '@gsap/react';
-import {SplitText} from 'gsap/all';
-
+import {SplitText, ScrollTrigger} from 'gsap/all';
+import { useMediaQuery } from 'react-responsive'; //use to detect screen size in js
+gsap.registerPlugin(useGSAP, ScrollTrigger, SplitText);
 const Hero = () => {
+  const videoRef = useRef(); //react hook that is used to directly access a DOM element
+//creates a React ref object --> a pointer to a DOM element
+  const isMobile = window.innerWidth <= 767;
   useGSAP(() => {
     const heroSplit = new SplitText('.title', {type: 'chars, words'});
     const paragraphSplit = new SplitText('.subtitle', {type: 'lines'});
@@ -36,48 +40,80 @@ const Hero = () => {
     })
       .to('.right-leaf', { y:200 }, 0)
       .to('.left-leaf', {y: -200}, 0)
+
+    const startValue = isMobile ? 'top 50%' : 'center 60%';
+    const endValue = isMobile ? '120%' : 'bottom top';
+
+    //video animation timeline
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'video',
+        start: startValue,
+        end: endValue,
+        scrub: true, //scroll position controls animation progress
+        pin: true, //video stays locked in place while the animation/page scroll progresses
+        // onEnter: () => videoRef.current.play(),
+        // onEnterBack: () => videoRef.current.play(),
+        // onLeave: () => videoRef.current.pause(),
+        // onLeaveBack: () => videoRef.current.pause(),
+      }
+    })
+      videoRef.current.onloadedmetadata = () => {
+        tl.to(videoRef.current, {
+          currentTime: videoRef.current.duration
+        })
+      }
   }, [])
   return (
-    <section id="hero" className ="noisy">
-      <h1 className="title">
-        MOJITO
-      </h1>
+    <>
+      <section id="hero" className="noisy">
+        <h1 className="title">MOJITO</h1>
 
-      <img
-        src="/images/hero-left-leaf.png"
-        alt="left-leaf"
-        className="left-leaf"
-      />
+        <img
+          src="/images/hero-left-leaf.png"
+          alt="left-leaf"
+          className="left-leaf"
+        />
 
-      <img
-        src="/images/hero-right-leaf.png"
-        alt="right-leaf"
-        className="right-leaf"
-      />
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="right-leaf"
+          className="right-leaf"
+        />
 
-      <div className='body'>
-        <div className="content">
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool. Crisp. Classic.</p>
+              <p className="subtitle">
+                Sip the Spirit <br /> of Summer
+              </p>
+            </div>
 
-          <div className = "space-y-5 hidden md:block">
-            <p>Cool. Crisp. Classic.</p>
-            <p className="subtitle">
-              Sip the Spirit <br /> of Summer
-            </p>
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktail on our menu is a blend of premium ingredients,
+                creative flair, and timeless recipes - designed to delight your
+                senses.
+              </p>
+              <a href="#cocktails">View Cocktails</a>
+            </div>
           </div>
-
-          <div className="view-cocktails">
-            <p className="subtitle">
-              Every cocktail on our menu is a blend of premium ingredients, creative
-              flair, and timeless recipes - designed to delight your senses.
-            </p>
-            <a href="#cocktails">View Cocktails</a>
-          </div>
-
         </div>
+      </section>
 
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/input.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
       </div>
 
-    </section>
-  )
+    </>
+  );
 }
 export default Hero;

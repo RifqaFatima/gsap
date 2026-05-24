7c7c4f2b7f4d693311e4c13ef34f1b04855ebaa6
RifqaFatima <rifqa88077@gmail.com>
2026-05-24 17:34:05 +0530

    implement the art section

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
 import Cocktails from './components/Cocktails.jsx';
 import About from './components/About.jsx';
+import Art from './components/Art.jsx';
 
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
@@ -17,6 +18,7 @@ const App = () => {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
     </main>
 
   )

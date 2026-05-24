dded7b3251254dc78c1f05cbccb32d227d40c429
RifqaFatima <rifqa88077@gmail.com>
2026-05-24 20:38:00 +0530

    Implemened the menu section

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero.jsx';
 import Cocktails from './components/Cocktails.jsx';
 import About from './components/About.jsx';
 import Art from './components/Art.jsx';
+import Menu from './components/Menu.jsx';
 
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
@@ -19,6 +20,7 @@ const App = () => {
       <Cocktails />
       <About />
       <Art />
+      <Menu />
     </main>
 
   )
